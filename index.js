86322eec029d62e5c6546325bf4522a777dde5db
abdullah <abdullah34alrafi@gmail.com>
2020-06-07 18:08:27 +0600

    Bot Online

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const token = 'NzE5MTI2MTc1NzkxOTcyNDQz.Xty7_w.a_uiJC6L96t85xYDFTduvsuoZw8';
+
+bot.on('ready', () =>{
+    console.log("Battle Bot is online!");
+})
+
+bot.login(token);
